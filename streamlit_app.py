81accbc0c524c25e9db7bc127f67ac3565518792
RAMANJEET SINGH SAINI <61779257+ramanjeetsaini@users.noreply.github.com>
2023-06-10 23:10:59 -0500

    Changing the location of list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,11 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+
 
 streamlit.title('My Parents new healthy diner')
 streamlit.header('Breakfast menu')
 streamlit.text('🐔 Butter Chicken')
 streamlit.text('🥣 Dal Tadka')
 streamlit.header(' Build your own smoothie')
+streamlit.dataframe(my_fruit_list)
