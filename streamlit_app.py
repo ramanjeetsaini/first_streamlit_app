446bed21d426732f1bc9755ff86400dcdcd62215
RAMANJEET SINGH SAINI <61779257+ramanjeetsaini@users.noreply.github.com>
2023-06-11 10:55:06 -0500

    Adding user input feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,13 @@ fruits_selected = streamlit.multiselect("Pick some fruits",list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
-
+streamlit.header("Fruityvice Fruit Advice")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 streamlit.text(fruityvice_response.json())
+fruit_choice = streamlit.text_input('What fruit would like information about?','Kiwi')
+streamlit.write('the user entered', fruit_choice)
+
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
